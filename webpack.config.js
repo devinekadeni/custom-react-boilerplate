406efe9dfec4c6153f7fb53b9979d18aa7c60499
Devin <devinekadeni@gmail.com>
2019-06-06 19:47:24 +0700

    added splitchunk optimization and sass-loader and miniCssExtractPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,30 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = ({ mode }) => {
+  const isDevelopment = mode === 'development'
+
   return {
     mode: mode,
-    devtool: 'inline-source-map',
+    devtool: isDevelopment ? 'inline-source-map' : '',
     devServer: {
       contentBase: '/dist',
     },
+    optimization: {
+      splitChunks: {
+        chunks: 'all',
+      },
+    },
     entry: {
       app: './src/index.js',
+      print: './src/print.js',
     },
     output: {
-      filename: '[name].bundle.js',
+      filename: '[name].[hash].bundle.js',
       path: path.resolve(__dirname, 'dist'),
-      publicPath: '/', // delete this if not running application on server
+      // publicPath: '/', // uncomment this if running application on server
     },
     module: {
       rules: [
@@ -26,7 +35,33 @@ module.exports = ({ mode }) => {
         },
         {
           test: /\.css$/,
-          use: ['style-loader', 'css-loader'],
+          use: [
+            isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+            'css-loader',
+          ],
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            {
+              loader: isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+            },
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true,
+                localIdentName: '[name]__[local]___[hash:base64:5]',
+                camelCase: true,
+                sourceMap: isDevelopment,
+              },
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: isDevelopment,
+              },
+            },
+          ],
         },
       ],
     },
@@ -34,6 +69,10 @@ module.exports = ({ mode }) => {
       new CleanWebpackPlugin(),
       new HtmlWebpackPlugin({
         template: './public/index.html',
+      }),
+      new MiniCssExtractPlugin({
+        filename: isDevelopment ? '[name].css' : '[name].[hash].css',
+        chunkFilename: isDevelopment ? '[id].css' : '[id].[hash].css',
       }),
     ],
   }
